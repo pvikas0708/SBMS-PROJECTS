fcbd5e14e5579ec3fdc3dadb23ef4f682a6af76f
pvikas0708 <pvikas0708@gmail.com>
2025-02-02 21:26:38 +0530

    Add files via upload
    commiting..

---
+++ MS-22-Spring-Security-Http-Basic-Auth-App-2/src/main/java/in/vikas/rest/MsgRestController.java
@@ -0,0 +1,26 @@
+package in.vikas.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class MsgRestController {
+
+	@GetMapping("/contact")
+	public String contact()
+	{
+		return "contact: +91 9988776644";
+	}
+	@GetMapping("/greet")
+	public String getGreetMsg() {
+		return "Good Morning...!";
+	}
+
+	@GetMapping("/welcome")
+	public String getWelcomeMsg() {
+		return "WelCome to Spring Security programming...!";
+
+	}
+}
